9a233e23c3dfa4061d0cf7645e83893d710d8eee
katjaryzhkova <e.ryzhkova@student.avans.nl>
2022-11-07 16:17:49 +0100

    Created folders, controllers and razor views

---
+++ WebApp/Controllers/ManagerController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace bumbo_projectgroep_j.Controllers
+{
+    public class ManagerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
