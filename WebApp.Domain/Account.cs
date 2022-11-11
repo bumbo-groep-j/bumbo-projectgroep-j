ab30b983e80ed47aaaae9846f85876158da32bea
Kevin Schuren <k.schuren@student.avans.nl>
2022-11-11 21:48:37 +0100

    Created LoginPage
    - Created LoginPage without backend functions.
    
    - LoginController
    
    - CSS File for the LoginPage
    
    - Made an class library called: "WebApp.Domain" where we will store our data.

---
+++ WebApp.Domain/Account.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApp.Domain
+{
+    public class Account
+    {
+        public int UserID { get; set; }
+        
+        [Required(ErrorMessage = "This field is required")]
+        public string Username { get; set; }
+
+        [DataType(DataType.Password)]  // Changes the input from normal characters to dots
+        [RegularExpression ("^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]){8,}$")]  // checks if there are a minimum of 8 characters consisting of at least 1 Capital letter and 1 Number
+        [Required(ErrorMessage = "This field is required")]
+        public string Password { get; set; } // Public for now for the database generation
+
+        public Account(string userName, string password)
+        {
+            Username = userName;
+            Password = password;
+        }
+
+    }
+}
---
+++ WebApp/Controllers/LoginController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bumbo.Controllers
+{
+    public class LoginController : Controller
+    {
+
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        // Check if username and password are correct
+        [HttpPost]
+        public ActionResult Verify(WebApp.Domain.Account model)
+        {
+            // microsoft authenticator????????????
+            return View();
+        }
+
+
+    }
+}
