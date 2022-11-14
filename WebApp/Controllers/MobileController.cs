9025f9899bc6b4bdeca3d31ec859ea4d1eebb4d7
katjaryzhkova <e.ryzhkova@student.avans.nl>
2022-11-14 19:06:53 +0100

    Added mobile detection and automatic redirection

---
+++ WebApp/Controllers/MobileController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Bumbo.Controllers
+{
+    public class MobileController : Controller
+    {
+        public IActionResult Availability()
+        {
+            ViewBag.IsMobile = true;
+            return View();
+        }
+
+        public IActionResult RequestLeave()
+        {
+            ViewBag.IsMobile = true;
+            return View();
+        }
+
+        public IActionResult SchoolSchedule()
+        {
+            ViewBag.IsMobile = true;
+            return View();
+        }
+
+        public IActionResult WorkSchedule()
+        {
+            ViewBag.IsMobile = true;
+            return View();
+        }
+
+        public IActionResult Index()
+        {
+            return RedirectToAction("WorkSchedule");
+        }
+    }
+}
