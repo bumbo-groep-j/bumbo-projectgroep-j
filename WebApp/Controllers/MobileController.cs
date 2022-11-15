aff7263d1155e45080cc59c7f02da0530a4b92e2
katjaryzhkova <e.ryzhkova@student.avans.nl>
2022-11-15 13:41:03 +0100

    Created mobile calendar

--- WebApp/Controllers/MobileController.cs
+++ WebApp/Controllers/MobileController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Bumbo.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Bumbo.Controllers
 {
@@ -26,6 +27,39 @@ namespace Bumbo.Controllers
         {
             ViewBag.IsMobile = true;
             return View();
+        }
+
+        public IActionResult InlineCalendar(
+            int month, int year,
+            int todayDay, int todayMonth, int todayYear,
+            int selectedDay, int selectedMonth, int selectedYear,
+            bool fullSize
+        )
+        {
+            int weekday = 1;
+            switch(new DateOnly(year, month, 1).DayOfWeek)
+            {
+                case DayOfWeek.Monday: weekday = 1; break;
+                case DayOfWeek.Tuesday: weekday = 2; break;
+                case DayOfWeek.Wednesday: weekday = 3; break;
+                case DayOfWeek.Thursday: weekday = 4; break;
+                case DayOfWeek.Friday: weekday = 5; break;
+                case DayOfWeek.Saturday: weekday = 6; break;
+                case DayOfWeek.Sunday: weekday = 7; break;
+            }
+
+            CalendarData data = new CalendarData
+            {
+                CurrentMonth = month,
+                CurrentYear = year,
+                FirstWeekdayOfMonth = weekday,
+                Today = new DateOnly(todayYear, todayMonth, todayDay),
+                Selected = new DateOnly(selectedYear, selectedMonth, selectedDay)
+            };
+
+            ViewBag.FullSize = fullSize;
+
+            return PartialView(data);
         }
 
         public IActionResult Index()
