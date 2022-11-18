ef1c3badea047c24bc1d1ec8ae9c9dc296cfd829
Thampie2185577 <p.anthonyrajah@student.avans.nl>
2022-11-18 13:04:54 +0100

    Fronted-end for workschedule.

--- WebApp/Controllers/EmployeeController.cs
+++ WebApp/Controllers/EmployeeController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System;
 
 namespace Bumbo.Controllers
 {
@@ -21,7 +23,30 @@ namespace Bumbo.Controllers
 
         public IActionResult WorkSchedule()
         {
+            //Get the current day
+            var dt = DateTime.Now;
+            var currentDay = dt.DayOfWeek;
+
+            //Get first and last day of the week
+            int begin = (7 + (dt.DayOfWeek - DayOfWeek.Monday)) % 7;
+            int end = (7 + (DayOfWeek.Sunday - dt.DayOfWeek)) % 7;
+
+            //Convert it to an integer
+            int days  = (int)currentDay -1;
+            //Subtract that many days from the current date.That gives you the date for Sunday.
+            //Create a list, starting with Sunday.
+            DateTime sunday = dt.AddDays(-days);
+            ViewBag.DaysThisWeek = Enumerable.Range(0, 7)
+                .Select(d => sunday.AddDays(d)) .ToList();
+
+            DateTime beginTime = dt.AddDays(-1 * begin).Date;
+            DateTime endTime = dt.AddDays(end).Date;
+            ViewBag.StartDate = beginTime;
+            ViewBag.EndDate = endTime;
+     
             return View();
-        }
+        }  
+        
+       
     }
 }
