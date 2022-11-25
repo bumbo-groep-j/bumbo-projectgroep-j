a88eeb4864d6ca21c14e941698f88af8266e62d9
Kevin Schuren <k.schuren@student.avans.nl>
2022-11-25 16:06:26 +0100

    CRUD Screens Employee
    - Scaffolded Create/Edit/Delete screen
    (Delete Screen not yet finished)
    - Added Regular expressions to Fields in the Employee class.
    - Added errormessage to the fields and in the required field so it gives error on wrong input or no input.
    - added back-end features to pages, so it can interact with database
    - added the page in the nav-bar under the name "Medewerkers"
    - in the CSS site file added input for button types to be the same as all other buttons

--- WebApp.Domain/Employee.cs
+++ WebApp.Domain/Employee.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
@@ -9,21 +10,31 @@ namespace WebApp.Domain
         [Key]
         public int Id { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Dit veld is verplicht")]
+        [RegularExpression("^(?<firstchar>(?=[A-Za-z]))((?<alphachars>[A-Za-z])|(?<specialchars>[A-Za-z]['-](?=[A-Za-z]))|(?<spaces> (?=[A-Za-z])))*$", 
+            ErrorMessage = "Onjuiste invoering")]
+        [DisplayName("Voornaam")]
         public string FirstName { get; set; }
 
+        [DisplayName("Tussenvoegsel")]
         public string? MiddleName { get; set; }
 
-        [Required]
+        [Required (ErrorMessage = "Dit veld is verplicht")]
+        [RegularExpression("^(?<firstchar>(?=[A-Za-z]))((?<alphachars>[A-Za-z])|(?<specialchars>[A-Za-z]['-](?=[A-Za-z]))|(?<spaces> (?=[A-Za-z])))*$",
+            ErrorMessage = "Onjuiste invoering")]
+        [DisplayName("Achternaam")]
         public string LastName { get; set; }
 
-        [Required]
+        [Required (ErrorMessage = "Dit veld is verplicht")]
         [Column(TypeName = "Date")]
+        [DisplayName("Geboortedag")]
         public DateTime DateOfBirth { get; set; }
 
         public string NFCToken { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Dit veld is verplicht")]
+        [RegularExpression ("^[1-9]\\d*$", ErrorMessage = "Onjuiste invoering: alleen cijfers ")]
+        [DisplayName("Uurloon")]
         public double HourlyWage { get; set; }
 
         [NotMapped]
---
+++ WebApp/Controllers/Manager_EmployeeCRUDController.cs
@@ -0,0 +1,106 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApp.Domain;
+
+namespace Bumbo.Controllers
+{
+    public class Manager_EmployeeCRUD : Controller
+    {
+        public ActionResult Index()
+        {
+            using (var context = new BumboDbContext())
+            {
+                return View(context.Employees.ToList());
+            }
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Employee employee)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    using (var context = new BumboDbContext())
+                    {
+                        context.Employees.Add(employee);
+                        context.SaveChanges();
+                    }
+                    return RedirectToAction(nameof(Index));
+                }
+                return View(employee);
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        public ActionResult Edit(int id)
+        {
+            try
+            {
+                using (var context = new BumboDbContext())
+                {
+                    return View(context.Employees.FirstOrDefault(b => b.Id == id));
+                }
+            }
+            catch (Exception)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Employee employee)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    using (var context = new BumboDbContext())
+                    {
+                        context.Attach(employee);
+                        context.Employees.Update(employee);
+                        context.Employees.Add(employee);
+                        context.SaveChanges();
+                    }
+                    return RedirectToAction(nameof(Index));
+                }
+                return View(employee);
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Manager_EmployeeCRUD/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Manager_EmployeeCRUD/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
