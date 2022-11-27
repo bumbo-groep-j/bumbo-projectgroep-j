ece61481ada4df9ff24a2119ee3057568cd2fade
Thampie2185577 <p.anthonyrajah@student.avans.nl>
2022-11-27 15:36:52 +0100

    prognosis pages get data from database and show on th page

--- WebApp/Controllers/ManagerController.cs
+++ WebApp/Controllers/ManagerController.cs
@@ -1,5 +1,6 @@
 ﻿using Bumbo.Models;
 using Microsoft.AspNetCore.Mvc;
+using WebApp.Domain;
 
 namespace Bumbo.Controllers
 {
@@ -10,8 +11,96 @@ namespace Bumbo.Controllers
             return View();
         }
 
-        public IActionResult Prognosis()
+        public void GetDate(DateTime date)
         {
+            //Get the current day
+            var dt = date;
+            //Get first and last day of the week
+            int begin = (7 + (dt.DayOfWeek - DayOfWeek.Monday)) % 7;
+            int end = (7 + (DayOfWeek.Sunday - dt.DayOfWeek)) % 7;
+
+            DateTime monday = dt.AddDays(-1 * begin).Date;
+            DateTime sunday = dt.AddDays(end).Date;
+
+            ViewBag.CurrentDate = dt;
+            ViewBag.StartDate = monday;
+            ViewBag.EndDate = sunday;
+        }
+
+       
+        public IActionResult SetNewDate(DateTime oldDate, string previousOrNext)
+        {
+            DateTime CurrentDate = new DateTime();
+            if (previousOrNext == "Next")
+            {
+                CurrentDate = oldDate.AddDays(+(int)oldDate.DayOfWeek + 6);
+            }
+            else if(previousOrNext == "Previous")
+            {
+                CurrentDate = oldDate.AddDays(-(int)oldDate.DayOfWeek - 6);
+            }
+        
+            return RedirectToAction("Prognosis", new { date = CurrentDate });
+        }
+
+        public void GetDaysOftheWeek(DateTime currentWeek)
+        {
+            List<DateTime> listOfDays = new List<DateTime>();
+            List<Prognosis> listOfData = new List<Prognosis>();
+
+            var now = currentWeek;
+            var currentDay = now.DayOfWeek;
+            int days = (int)currentDay;
+            DateTime sunday = new DateTime();
+
+            if (days == 0)
+            {
+                days = 7;
+                sunday = now.AddDays(-days);
+            }
+            else
+            {
+                sunday = now.AddDays(-days);
+            }
+            
+            listOfDays = Enumerable.Range(1, 7)
+                           .Select(d => sunday.AddDays(d))
+                           .ToList();
+
+            using (var ctx = new BumboDbContext())
+            {
+                var prognoses = ctx.Prognosis;
+
+                foreach (var data in prognoses)
+                {
+                    DateTime dbDate = data.Date;
+                    foreach (DateTime date in listOfDays)
+                    {
+                        if (dbDate.Date == date.Date)
+                        {
+                            listOfData.Add(data);
+                        }
+                    }
+                }
+            }
+
+            ViewBag.Data = listOfData;
+            ViewBag.Days = listOfDays;
+        }
+
+        public IActionResult Prognosis(DateTime? date)
+        {
+            DateTime dateTime = new DateTime();
+            dateTime =  (!date.HasValue) ? DateTime.Now : date.Value;
+            GetDate(dateTime);
+            GetDaysOftheWeek(dateTime);
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Prognosis(string totalColli, string totalCustomers)
+        {
+            
             return View();
         }
 
