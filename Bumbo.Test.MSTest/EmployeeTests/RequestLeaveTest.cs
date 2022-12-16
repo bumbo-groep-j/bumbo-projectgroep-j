25515c4d40b8bd8ebd3b9ccd9f0d1f4019b54ed9
Kevin Schuren <k.schuren@student.avans.nl>
2022-12-16 15:28:02 +0100

    Test for leave request
    dont know if this is good, so it will change

--- Bumbo.Test.MSTest/EmployeeTests/RequestLeaveTest.cs
+++ Bumbo.Test.MSTest/EmployeeTests/RequestLeaveTest.cs
@@ -15,6 +15,8 @@ namespace Bumbo.Test.MSTest.EmployeeTests
     [TestClass]
     public class RequestLeaveTest
     {
+        // No idea, if this is good for testing
+        
         [TestMethod]
         public void RequestLeaveSavedToDB_Succes()
         {
@@ -40,5 +42,46 @@ namespace Bumbo.Test.MSTest.EmployeeTests
             mockContext.Verify(context => context.LeaveRequests.Add(leaveRequest), Times.Once());
             mockContext.Verify(context => context.SaveChanges(), Times.Once());
         }
+
+        [TestMethod]
+        public void RequestLeaveLinkedUser_Succes()
+        {
+            // Arrange
+            var leaveRequest = new LeaveRequest
+            {
+                EmployeeId = 1,
+                StartDate = DateTime.Now,
+                EndDate = DateTime.Now.AddDays(1),
+                Comment = "Test",
+                Approved = false,
+                Rejected = false
+            };
+
+            // make employee object with all the properties
+            var employee = new Employee
+            {
+                Id = 1,
+                UserName = "username",
+                FirstName = "FirstName",
+                LastName = "Test",
+                DateOfBirth = DateTime.Now,
+                NFCToken = "123456789",
+                HourlyWage = 10,
+            };
+            
+            leaveRequest.EmployeeId = employee.Id;
+
+            // Act 
+            var mockContext = new Mock<BumboDbContext>();
+            var mockUserManager = new Mock<UserManager<Account>>();
+            var controller = new EmployeeController(mockContext.Object, mockUserManager.Object);
+
+            controller.RequestLeave(leaveRequest);
+            
+            // Assert
+            Assert.AreEqual(leaveRequest.EmployeeId, employee.Id);
+
+        }
     }
 }
+
